2fa9f638c73a45c0d0fe98741333a9dc452f5990
dousa <dsousa@adobe.com>
2020-02-24 18:57:32 -0800

    fix

--- server/src/database.js
+++ server/src/database.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require(process.env.SERVICE_KEY_PATH);
+const serviceAccount = require('./service_key.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
