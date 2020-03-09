e03bc5689ba440c0378e802dfbecc25e2c766430
dousa <dsousa@adobe.com>
2020-03-09 09:22:04 -0700

    update server

--- server/src/database.js
+++ server/src/database.js
@@ -1,10 +1,13 @@
-const admin = require("firebase-admin");
+const admin = require('firebase-admin');
 
 const serviceAccount = require(process.env.SERVICE_KEY);
 
+console.log('test');
+console.log(process.env.SERVICE_KEY);
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://pmtool-9af96.firebaseio.com"
+  databaseURL: 'https://pmtool-9af96.firebaseio.com',
 });
 
 const database = admin.database();
