96d4a94ad776f5ca55e34d5b50cab61bcb040884
dousa <dsousa@adobe.com>
2020-03-09 15:08:38 -0700

    update deployment script

--- server/deploy.sh
+++ server/deploy.sh
@@ -11,12 +11,12 @@ rm pm_board_server.tar.gz
 
 echo 'entering remote server...'
 ssh dsousa@danielmooncloud.com << 'ENDSSH'
-pm2 stop pm_board_server
+pm2 stop ecosystem.config.js
 rm -rf pm_board_server
 mkdir pm_board_server
 tar xf pm_board_server.tar.gz -C pm_board_server
 cd pm_board_server
 yarn install
 cd ../
-
+pm2 start ecosystem.config.js
 ENDSSH
